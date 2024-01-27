0631dfa1e5f7699afa833195d99ddf44a636a6b4
Claudio Rea <claudiorea@Claudios-MacBook-Air.local>
2024-01-27 20:02:03 +0100

    chagned localc

--- pages/rules.js
+++ pages/rules.js
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import Image from 'next/image';
-import styles from './Rules.module.css';
+import styles from '../styles/Rules.module.css';
 
 export default function Rules() {
     return (
