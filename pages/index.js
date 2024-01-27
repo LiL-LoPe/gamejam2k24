d79663b28f473a76703a915e93d902a573281c32
LiL-LoPe <117383608+LiL-LoPe@users.noreply.github.com>
2024-01-27 13:26:54 +0100

    errore rosso e camera percentuale

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,25 @@
 import { useAppContext } from '@/contexts/AppContext';
 import Link from 'next/link';
+import { useState } from 'react';
 
 export default function Home() {
-
   const { players, updateString, addPlayer, removePlayer } = useAppContext();
+  const [validationError, setValidationError] = useState(false);
 
   const handleInputChange = (index, event) => {
     const newValue = event.target.value;
     updateString(index, newValue);
+  };
+
+  const playButtonHandler = () => {
+    const filledPlayers = players.filter(player => player.trim() !== '');
+    if (filledPlayers.length < 3) {
+      setValidationError(true);
+    } else {
+      setValidationError(false);
+      // Navigate to the next page programmatically
+      window.location.href = '/rules';
+    }
   };
 
   return (
@@ -15,25 +27,22 @@ export default function Home() {
       <div className='flex flex-col h-screen items-center w-10/12'>
         <h1 className=''>Iscrizione Giocatori</h1>
         {players.map((str, index) => (
-        <div key={index}>
-          <label>
-            Player {index + 1}:
-            <input
-              type="text"
-              value={str}
-              onChange={(event) => handleInputChange(index, event)}
-              className='w-full'
-            />
-          </label>
-        </div>
-      ))}
+          <div key={index} >
+            <label>
+              Player {index + 1}:
+              <input
+                type="text"
+                value={str}
+                onChange={(event) => handleInputChange(index, event)}
+                className={`w-full ${validationError ? 'error' : ''}`}
+              />
+            </label>
+          </div>
+        ))}
+        {validationError &&  <p style={{ fontSize: '0.65em' }} className={`text-red-500 ${validationError ? 'error-message' : ''}`}>Almeno tre giocatori devono essere inseriti.</p>}
         <button onClick={addPlayer}>+</button>
         <button onClick={removePlayer}>-</button>
-        <Link
-            className='mt-10' 
-            href={{ pathname: '/rules'}}>
-          Gioca
-        </Link>
+        <button onClick={playButtonHandler}>Gioca</button>
       </div>
     </main>
   );
