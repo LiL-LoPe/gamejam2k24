304b33ed1f350f5ceb71938b525d1c3ac8487bff
Claudio Rea <claudiorea@Claudios-MacBook-Air.local>
2024-01-27 21:34:45 +0100

    creati bottoni

--- pages/rules.js
+++ pages/rules.js
@@ -1,22 +1,27 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import styles from '../styles/Rules.module.css';
+import SettingButton from './SettingButton';
+import RulesButton from './RulesButton';
+import BackButton from './BackButton';
+
 
 export default function Rules() {
     return (
         <main className={styles.main}>
+            <SettingButton />
+            <RulesButton   />
+            <div className={styles.rulesButton}></div>
             <div className={styles.imageContainer}>
                 <Image 
                     src="/Images/Regolamento.png" 
                     alt="Descrizione immagine" 
-                    width={380}  // Imposta la larghezza desiderata
+                    width={500}  // Imposta la larghezza desiderata
                     height={1000} // Imposta l'altezza desiderata 
                     objectFit="contain" 
                     className={styles.image}
                 />
-                <Link href="/prossimaPagina" className={styles.nextButton}>
-                    Avanti
-                </Link>
+                <BackButton className={styles.nextButton} />
             </div>
         </main>
     );
